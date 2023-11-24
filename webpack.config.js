0447889863062c4236bd849040f5546b94f775e9
Patrick-Tomic <pat12tomic@gmail.com>
2023-11-24 14:11:20 -0500

    try again

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 /* eslint-disable no-undef */
-import { resolve } from 'path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+ const path = require('path')
+ import HtmlWebpackPlugin from 'html-webpack-plugin';
 export const mode = 'development';
 export const entry = { bundle: resolve(__dirname, 'src/index.js') };
-export const devtool = 'inline-source-map';
+export const devtool = 'inline-source-map'; 
 export const devServer = {
+	
 	static: './dist',
 };
 export const plugins = [
